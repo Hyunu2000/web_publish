a411402049f83894fc779a2430943250a0c9e23e
kang <rkdgusdn0319@gmail.com>
2024-11-06 16:27:25 +0900

    commit

---
+++ javascript/1_variable.js
@@ -0,0 +1,32 @@
+/**
+ *  변수 형식 : var, let, const
+ *  형식 : (var, let, const) 변수명 = 데이터값;
+ */
+
+// name 이라는 변수를 선언하고 홍길동 이름으로 초기화합니다.
+// age 변수를 선언하고, 20으로 초기화
+let name = '홍길동';
+let age = 20;
+const address = '서울시';
+
+// name을 현우로 바꾼다.
+name = '현우';
+age = '서울시';
+age = '30';
+
+//  const로 선언된 변수는 재할당이 불가능합니다. 즉, 변수에 값을 한 번 할당한 후에는 그 값을 변경할 수 없다.
+/**
+ * EX)
+ *  const a = 10;
+ *  a = 20;  // TypeError: Assignment to constant variable.
+ */
+
+
+
+// let age = '40';  <<< 문법적으로 불가하다. let으로 변수를 재선언할 수 없다.
+
+// name, age 변수의 값을 콘솔에 출력
+console.log(name);
+console.log(age);
+
+console.log(typeof'age');
---
+++ javascript/4_object.js
@@ -0,0 +1,31 @@
+// object : Array(배열 : []), JSON({}) ..
+let apple = null;
+apple = {
+    name: 'apple',
+    color: 'red',
+    display: '🍎'
+};
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+console.log('----------');
+
+let grape = {
+    name: 'grape',
+    color: 'violet',
+    display: '🍇'
+};
+
+console.log(grape);
+console.log(grape.name);
+console.log(grape.color);
+console.log(grape.display);
+
+// 1~5까지 출력
+let numberList = [1, 2, 3, 4, 5];
+console.log(numberList);
+console.log(numberList[0]);
+console.log(numberList[4]);
