2f9a589ca2d8464547d95749e90aa4ab8057d116
kang <rkdgusdn0319@gmail.com>
2024-11-15 13:36:52 +0900

    commit

---
+++ javascript/7.built-in/1_wrapper.js
@@ -0,0 +1,36 @@
+// 기본 데이터타입(Primitive DataType) - number, string, boolean
+// 기본 데이터타입을 클래스로 정의한 것을 wrapper 클래스라 함
+
+/** Number class */
+let a = "123";
+if(typeof a === "string") a = parseInt(a);
+if(a === 123) console.log(a);
+
+if(Number("123") === 123)console.log(`a => ${a}`);
+
+let maxValue = Number.MAX_VALUE;
+let minValue = Number.MIN_VALUE;
+console.log(maxValue);
+console.log(minValue);
+
+let x = 100;
+let y = new Number(100); 
+console.log(typeof x);
+console.log(typeof y);
+console.log(x === y); // x는 콜스택의 넘버타입 y는 힙메모리의 주소를 가진 오브젝트타입
+console.log(x === y.valueOf());
+
+/** String class + template literal(``) */
+let str = "hello~";
+let str2 = new String("hello~");
+console.log(typeof str);
+console.log(typeof str2);
+console.log(str === str2);
+console.log(str === str2.valueOf());
+
+/** Boolean class */
+let flag = true;
+let flag2 = new Boolean(true);
+console.log(typeof flag);
+console.log(typeof flag2);
+console.log(flag === flag2.valueOf());
