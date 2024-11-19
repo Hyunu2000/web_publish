80d73b986276915f03e781789ee69d9de00e9f35
kang <rkdgusdn0319@gmail.com>
2024-11-19 17:12:24 +0900

    2024-11-19 study

---
+++ javascript/9.iterable/3_spread.js
@@ -0,0 +1,40 @@
+// ...(Spread Operator, 전개구문)
+// 모든 Iterable object에서 사용 가능
+// function 함수명(...param)
+// [ ...iterable ]
+// { ...iterable }
+
+// 1. function 함수명(...param)
+function add(...numbers) { // ...numbers[파라미터..] <-- 배열
+    let sum = 0;
+    for(number of numbers) sum += parseInt(number); // 배열에 string 타입으로 넘어올 때를 감안
+    return sum;
+}
+
+let sum = add(1, 2, 3, 4, 5, '6');
+console.log(`sum = ${sum}`);
+
+// 2. [ ...iterable ]
+let fruit1 = ['🍏', '🍋'];
+let fruit2 = ['🍓', '🍊', '🍉'];
+
+// fruit1과 fruit2 사이에 '🍍' 추가
+let fruit3 = fruit1.concat('🍍', fruit2);
+let fruit4 = [...fruit1, '🍍', ...fruit2]; // fruit1, 2의 모든 요소를 가져옴 **...XXX 자주사용함**
+
+console.log(`fruit3 = ${fruit3}`);
+console.log(`fruit4 = ${fruit4}`);
+
+// 3. { ...iterable }
+const hong = {
+    name: "홍길동",
+    age: 20,
+    address: "서울시 강남구"
+};
+
+const hongUpdate = {
+    ...hong,
+    job: "개발자"
+}
+
+console.log(hongUpdate);
---
+++ javascript/9.iterable/6_map2.js
@@ -0,0 +1,61 @@
+// Map 클래스 생성
+const fList = new Map([[ 1, 'one' ],[ 2, 'two' ]]);
+console.log(fList);
+console.log(fList.get(1));
+console.log(fList.get(2));
+
+// 사이즈 확인
+console.log(`size ==> ${fList.size}`);
+
+// 데이터 추가
+fList.set(3, 'three');
+// fList.set([[4, 'four'], [5, 'five']]); // set은 key, vauvale
+
+console.log(fList);
+
+// 데이터 확인
+if(fList.has(3)) console.log(fList.get(3));
+
+// 모든 키값 출력
+console.log(fList.keys());
+
+// 모든 value 출력
+console.log(fList.values());
+
+// key, value를 입력된 순서대로 배열 형태로 반환
+let iteratorObj = fList.entries();
+console.log(iteratorObj);
+// [Map Entries] { [ 1, 'one' ], [ 2, 'two' ], [ 3, 'three' ] }
+
+// console.log(iteratorObj.next());
+// { value: [ 1, 'one' ], done: false }
+console.log(iteratorObj.next().value);
+// [ 1, 'one' ]
+
+// 순회 : forEach value가 먼저 출력된다. 받을 수 있는 인자는 value, key, map 3개 뿐
+fList.forEach((element) => console.log(element));
+fList.forEach((value, key, map) => 
+    console.log(value, key, map));
+
+// 순회 : for...of
+// [key, value]
+for(let element of fList) console.log(element);
+
+// key만 출력
+for(let key of fList.keys()) console.log(`key => ${key}`); // key => 1
+
+// value만 출력
+for(let value of fList.values()) console.log(`value => ${value}`); // value => one
+
+// entries
+for(let entry of fList.entries()) console.log(`entry => ${entry}`); // entry => 1,one
+
+// for(let entry of fList.entries()) console.log(`entry => ${entry.value()}`); // 사용불가
+
+// 데이터 삭제
+if(fList.has(1)) fList.delete(1);
+console.log(fList);
+
+// 전체 삭제
+fList.clear();
+console.log(fList);
