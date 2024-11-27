9e080b785c81965794a2a5c0461eedd10e705201
kang <rkdgusdn0319@gmail.com>
2024-11-27 17:54:48 +0900

    git commit -m "11-27 study"

--- javascript/13.json/kobis.js
+++ javascript/13.json/kobis.js
@@ -1,4 +1,6 @@
-import { kobisBoxOffice  as boxOffice } from "./kobisCommons.js";
+import { kobisBoxOffice as boxOffice, 
+        searchMoviePoster, 
+        kmdbMovieDetail } from "./kobisCommons.js";
 
 
 initForm();
@@ -16,6 +18,13 @@ function initForm() {
             <button type="button" id="searchBtn">Search</button>
         </div>
         <div id="result"></div>
+
+        <div id="imageModal" class="modal">
+            <div class="modal-content">
+                <span id="closeModal" class="close">&times;</span> 
+                <img id="modalImage" src="" alt="Modal Image"> 
+            </div>
+        </div>
     `;
 
     document.querySelector("body").innerHTML = output; // 이벤트는 아웃풋이 실행된 후
@@ -31,15 +40,13 @@ function initForm() {
         let searchDt = document.querySelector("#searchDt");
 
         // if를 긍정으로 보고 else를 부정으로 보는 방법도 좋아보인다. if에 원활하게 작동 가능한 경우만
-        
-
-        if(type.value === 'Default') {
+        if (type.value === 'Default') {
             alert('박스오피스 타입을 선택해주세요.');
             type.focus();
-        } else if(searchDt.value === '') {
+        } else if (searchDt.value === '') {
             alert('검색일자를 입력해주세요.');
             searchDt.focus();
-        } else if(searchDt.value.length != 8) {
+        } else if (searchDt.value.length != 8) {
             alert('입력 형식은 기호나 띄어쓰기 없이 숫자 8자리 입니다.')
         }
         else {
@@ -58,52 +65,164 @@ function initForm() {
  */
 function searchBoxOffice(ktype, searchDt) {
     boxOffice(ktype, searchDt) // Promise 객체로 리턴
-                .then((result) => {
-                    const ranktype = ktype.toLowerCase();
+        .then((result) => {
+            const ranktype = ktype.toLowerCase();
 
-                    const type = result.boxOfficeResult.boxofficeType;
-                    const range = result.boxOfficeResult.showRange;
+            const type = result.boxOfficeResult.boxofficeType;
+            const range = result.boxOfficeResult.showRange;
+            let rankList = null; // ktype을 체크하여 Daily, Weekly
+            let posterList = [];
 
-                    // ktype을 체크하여 Daily, Weekly
-                    let rankList = null;
-                    if(ktype === 'Daily') {
-                        rankList = result.boxOfficeResult.dailyBoxOfficeList;
-                    } else if(ktype === 'Weekly') {
-                        rankList = result.boxOfficeResult.weeklyBoxOfficeList;
-                    }
+            if (ktype === 'Daily') {
+                rankList = result.boxOfficeResult.dailyBoxOfficeList;
+            } else if (ktype === 'Weekly') {
+                rankList = result.boxOfficeResult.weeklyBoxOfficeList;
+            }
+
+            // 영화 포스터 가져오기 
+            rankList.forEach((element) => {
+                let movieNm = element.movieNm;
+                let openDt = element.openDt.replaceAll('-', ''); // 요청인자 확인하고 그 양식에 맞게
+
+                posterList.push(getPoster(movieNm, openDt));
+            });
+
+            Promise.all(posterList) // 비동기식 처리는 모두 종료가 되도록 실행
+                .then((poster) => {
+
                     let output = `
-                        <h5>박스오피스 타입 : ${type}</h5>
-                        <h5>박스오피스 일자 : ${range}</h5>
-                        <table border=1>
-                            <tr>
-                                <th>순위</th>
-                                <th>제목</th>
-                                <th>개봉일</th>
-                                <th>당일관객수</th>
-                                <th>누적관객수</th>
-                            </tr>`;
-
-                        rankList.forEach((element) => {
-                            output += `
+                            <h5>박스오피스 타입 : ${type}</h5>
+                            <h5>박스오피스 일자 : ${range}</h5>
+                            <table border=1>
                                 <tr>
-                                    <td>${element.rank}</td>
-                                    <td>${element.movieNm}</td>
-                                    <td>${element.openDt}</td>
-                                    <td>${element.audiCnt}</td>
-                                    <td>${element.audiAcc}</td>
-                                </tr>
-                            `;
-                        });
+                                    <th>순위</th>
+                                    <th>포스터</th>
+                                    <th>제목</th>
+                                    <th>개봉일</th>
+                                    <th>당일관객수</th>
+                                    <th>누적관객수</th>
+                                </tr>`;
 
-                        output += `</table>`;
+                    rankList.forEach((element, i) => {
+                        output += `
+                                    <tr>
+                                        <td>${element.rank}</td>
+                                        <td><img src=${poster[i]} width="100px" class="poster"
+                                            id="${element.movieNm},${element.openDt.replaceAll('-', '')}"></td>
+                                        <td>${element.movieNm}</td>
+                                        <td>${element.openDt}</td>
+                                        <td>${element.audiCnt}</td>
+                                        <td>${element.audiAcc}</td>
+                                    </tr>
+                                `;
+                    });
 
-                        // console.log(output);
-                        
-                        // 테이블 화면 출력
-                        document.querySelector("#result").innerHTML = output;
-                        
-                })
-                .catch();
+                    output += `</table>`;
 
+                    // 테이블 화면 출력
+                    document.querySelector("#result").innerHTML = output;
+
+
+                    // 이미지 클릭 이벤트
+                    const images = document.querySelectorAll(".poster");
+                    images.forEach((image) => image.addEventListener('click', onMovieDetail));
+
+
+                }).catch(); // Promise.all()
+
+            })
+            .catch(); //        
 
 };
+
+/** 이미지 이벤트 처리 함수 */
+// function onMovieDetail(event) {
+//     const modal = document.querySelector('#imageModal');
+//     const modalImage = document.querySelector('#modalImage');
+//     const closeModalBtn = document.querySelector('#closeModal');
+
+
+
+//     let [movieNm, openDt] = event.target.id.split(",");
+
+//     kmdbMovieDetail(movieNm, openDt)
+//         .then((result) => {
+//             // console.log(result.Data[0].Result[0].title.replaceAll(' !HS ', '').replaceAll(' !HE ', ''));
+//             const imageSrc = event.target.src; // 클릭한 이미지의 src를 가져옴
+//             modalImage.src = imageSrc; // 모달 창에 이미지 넣기
+//             modal.style.display = 'block'; // 모달 창을 표시
+            
+//         })
+//         .catch((error) => console.log(error));
+
+//         // 모달 닫기 버튼 클릭 시 모달 닫기
+//         closeModalBtn.addEventListener('click', () => {
+//             modal.style.display = 'none'; // 모달 창 닫기
+//         });
+
+//         // 모달 바깥쪽 클릭 시 모달 닫기
+//         window.addEventListener('click', () => {
+//             modal.style.display = 'none'; // 모달 창 닫기
+//         });
+
+// } // onMovieDetail
+
+
+
+
+
+
+
+
+/** 이미지 이벤트 처리 함수 */
+function onMovieDetail(event) {
+    const modal = document.querySelector('#imageModal');
+    const modalImage = document.querySelector('#modalImage');
+    const modalContent = document.querySelector('.modal-content');
+    const closeModalBtn = document.querySelector('#closeModal');
+    
+    let [movieNm, openDt] = event.target.id.split(",");
+    
+    kmdbMovieDetail(movieNm, openDt)
+    .then((result) => {
+        const movieInfo = result.Data[0].Result[0];
+        const title = movieInfo.title.replaceAll('!HS', '').replaceAll('!HE', '');
+        const directors = movieInfo.directors.director.map(d => d.directorNm).join(", ");
+        const actors = movieInfo.actors.actor.map(a => a.actorNm).join(", ");
+        const plot = movieInfo.plots.plot[0].plotText;
+        
+        const movieDetailHtml = `
+        <h2>${title}</h2>
+        <p><strong>감독:</strong> ${directors}</p>
+        <p><strong>출연:</strong> ${actors}</p>
+        <p><strong>줄거리:</strong> ${plot}</p>
+        `;
+        
+        modalImage.src = event.target.src; // 클릭한 이미지의 src를 가져옴
+        modalContent.insertAdjacentHTML('beforeend', movieDetailHtml); // 영화 상세 정보를 추가
+        modal.style.display = 'block'; // 모달 창을 표시
+    })
+    .catch((error) => console.log(error));
+    
+    // 모달 닫기 버튼 클릭 시 모달 닫기
+    closeModalBtn.addEventListener('click', () => {
+        modal.style.display = 'none'; // 모달 창 닫기
+        modalContent.querySelectorAll('h2, p').forEach(element => element.remove()); // 기존 영화 상세 정보 삭제
+    });
+    
+    // 모달 바깥쪽 클릭 시 모달 닫기
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none'; // 모달 창 닫기
+            modalContent.querySelectorAll('h2, p').forEach(element => element.remove()); // 기존 영화 상세 정보 삭제
+        }
+    });
+}
+
+
+
+
+/** 순차적으로 비동기식 호출을 위해 getPoster 함수 생성 */
+async function getPoster(movieNm, openDt) {
+    return await searchMoviePoster(movieNm, openDt);
+} // getPoster
--- javascript/13.json/kobisCommons.js
+++ javascript/13.json/kobisCommons.js
@@ -1,19 +1,20 @@
+
 /**
  * KOBIS 박스오피스 정보 호출 함수
- */
+*/
 export async function kobisBoxOffice(type, searchDt) {
     const key = `fee1a09134df077267d06fa7f63aae37`;
     const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/search${type}BoxOfficeList.json?key=${key}&targetDt=${searchDt}`;
-
+    
     const kobis = await fetch(url);
     const jsonData = await kobis.json();
-
+    
     return jsonData;
 }
 
 /**
  * KOBIS 영화 리스트 호출 함수
- */
+*/
 export async function kobisMovieList() {
     const key = `fee1a09134df077267d06fa7f63aae37`;
     const curPage = 1;
@@ -22,19 +23,46 @@ export async function kobisMovieList() {
     
     const movieList = await fetch(url);
     const jsonData = await movieList.json();
-
+    
     return jsonData;
 }
 
 /**
  * KOBIS 영화 정보 상세 호출 함수
- */
+*/
 export async function kobisMovieDetail(movieCd) {
     const key = `fee1a09134df077267d06fa7f63aae37`;
     const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${key}&movieCd=${movieCd}`;
     
     const movie = await fetch(url);
     const jsonData = await movie.json();
+    
+    return jsonData;
+}
+
+/**
+ * KMDB 영화 포스터 검색
+ */
+export async function searchMoviePoster(movieNm, openDt) {
+    const key = `C8S353S3UMU8P7M4535B`;
+    const url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&ServiceKey=${key}&title=${movieNm}&releaseDts=${openDt}`;
+
+    const result = await fetch(url);
+    const jsonData = await result.json();
+
+    return jsonData.Data[0].Result[0].posters.split("|")[0];
+}
+
+/**
+ * KMDB 영화 상세 정보
+*/
+
+export async function kmdbMovieDetail(movieNm, openDt) {
+    const key = `C8S353S3UMU8P7M4535B`;
+    const url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&ServiceKey=${key}&title=${movieNm}&releaseDts=${openDt}`;
+
+    const result = await fetch(url);
+    const jsonData = await result.json();
 
     return jsonData;
 }
