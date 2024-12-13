364aa498a672f66f2bb6ad14642db3f77c202d59
kang <rkdgusdn0319@gmail.com>
2024-12-13 17:42:29 +0900

    git commit -m "12-13 study"

--- react/basic-state-app/src/index.js
+++ react/basic-state-app/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppAladin from './components/aladin/AppAladin.jsx';
-// import AppOlive from './components/olive/AppOlive.jsx';
-// import AppCounter2 from './components/counter2/AppCounter2.jsx';
+import AppForm from './components/form/AppForm.jsx';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppAladin />
+    <AppForm />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
