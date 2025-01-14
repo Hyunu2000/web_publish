92f37af259d1541eb2cfe7ac0470cb19caccc1da
kang <rkdgusdn0319@gmail.com>
2025-01-14 17:05:31 +0900

    git commit -m "01-14 study"

---
+++ shoppy2/client2/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className='footer-outer'>
+            <div className='footer'>
+                <p>
+                    ㈜2000코리아
+                    대표자 : 강현우
+                    주소 : 서울특별시 강남구 어디로 231, west동 200층(역삼동, 라이트필드)
+                </p>
+
+                <p>
+                    사업자등록번호 : 23424-234234-19345675
+                    통신판매업신고 : 제2637489호-서울강남-87987호 사업자정보확인
+                    개인정보보호책임자 : 강현우
+                    호스팅서비스 : ㈜2000코리아
+                    고객센터 : 0000-0000
+                </p>
+
+                <p>
+                    COPYRIGHT ⓒ ㈜2000코리아 ALL RIGHTS RESERVED.
+                </p>
+            </div>
+        </div>
+    );
+}
+
